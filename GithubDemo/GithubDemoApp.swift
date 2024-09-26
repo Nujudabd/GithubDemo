fb53a145375acae0f4fcece025f011b65a0a558f
Nujud <Nujudalsaad>
2024-09-26 16:54:51 +0300

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Nujud Abdullah on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
